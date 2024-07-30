3fcca488df8294daf50df5f121412382245916fa
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2024-07-30 22:08:52 +0300

    homepage-start

--- serene-minds/app/page.tsx
+++ serene-minds/app/page.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Carousel from './Carousel'
+import { cardText } from './card-text'
+import CardWithText from './CardWithText'
 
-const page = () => {
+export default function Homepage() {
   return (
-    <div>page</div>
+    <>
+    <Carousel />
+        <div className='text-center m-5'>Homepage</div>
+        <div className='flex gap-3 '>
+      {
+        cardText.map((card, index) => (
+          <CardWithText key={index} {...card} />
+        ))
+      }
+</div>
+    </>
   )
 }
 
-export default page
+
